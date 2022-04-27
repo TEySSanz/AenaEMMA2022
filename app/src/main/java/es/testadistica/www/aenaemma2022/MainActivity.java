52243b523c4b0f3657dd90094557740b26508d79
Pintos <epintos@trecursos.org>
2022-04-27 11:19:00 +0200

    27/04/2022 11:17 - Actualización de ficheros de configuración y creación de la utilidad para el SearchableSpinner

--- app/src/main/java/es/testadistica/www/aenaemma2022/MainActivity.java
+++ app/src/main/java/es/testadistica/www/aenaemma2022/MainActivity.java
@@ -1,8 +1,19 @@
 package es.testadistica.www.aenaemma2022;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import es.testadistica.www.aenaemma2022.utilidades.SearchableSpinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SearchableSpinner spB3_LIT = (SearchableSpinner) this.findViewById(R.id.survey_spinner_B3_LIT);
+        List<String> adapter = new ArrayList<>();
+
+        adapter.add(0,"Casa");
+        adapter.add(1,"Perro");
+        adapter.add(2,"Hospital");
+        ArrayAdapter<String> b3Adapter = new ArrayAdapter<String>(this, R.layout.selection_spinner_item_small, adapter);
+        spB3_LIT.setAdapter(b3Adapter);
+        spB3_LIT.setTitle("HOLA");
+        spB3_LIT.setPositiveButton("CLARO QUE SI");
     }
 }
